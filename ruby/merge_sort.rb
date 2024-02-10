ccdedf1ca12c09ae38e0127e04df9315ad41ee80
Yaugesha <eugene.korchemenko@gmail.com>
2024-02-10 17:32:14 +0300

    refactor: change implementation of meging l and r arrays

--- ruby/merge_sort.rb
+++ ruby/merge_sort.rb
@@ -7,24 +7,23 @@ def merge_sort arr
 
   new_arr = []
   return arr if length == 1
-  left_arr.each do |left_el|
-    left_el_pushed = false
-    right_arr.each do |right_el|
-      if right_el <= left_el
-        new_arr << right_el
-        right_arr.delete(right_el)
-      else
-        new_arr << left_el
-        left_el_pushed = !left_el_pushed
-        break
-      end
+  left_i = 0
+  right_i = 0
+  while left_i < left_arr.length
+    if right_i < right_arr.length && right_arr[right_i] <= left_arr[left_i]
+      new_arr << right_arr[right_i]
+      right_i += 1
+    else
+      new_arr << left_arr[left_i]
+      left_i += 1
     end
-    new_arr << left_el unless left_el_pushed
   end
-  right_arr.each { |el| new_arr << el}
+  new_arr.concat(right_arr[right_i..right_arr.length-1]) if right_i < right_arr.length
 
   new_arr
 end
+
+#thanks CS50
 
 p merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
 p merge_sort([105, 79, 100, 110])
