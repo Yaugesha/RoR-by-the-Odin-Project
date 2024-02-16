ee33c44d0bc723eb380294001d31f58e8cf014ba
yaugesha <eugene.korchemenko@gmail.com>
2024-02-16 09:55:15 +0300

    feat: start micro-reddit project and add user model

---
+++ rails/micro-reddit/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :login, length: {in: 4..20}
+  validates :password, presence: true,
+                       confirmation: true,
+                       length: { in: 6..20, message: "must have length between 4 and 20 characters" },
+                       format: { with: /\A(?=.*\d)(?=.*[[:punct:]])[\w\d[:punct:]]+\z/,
+                                 message: "must contain at least one digit and one special character" }
+  validates :password_confirmation, presence: true
+end
