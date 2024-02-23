49a8bba9d56becbfe95200305ce6cd666965562d
yaugesha <eugene.korchemenko@gmail.com>
2024-02-23 23:54:32 +0300

    feat: add displaying search results

--- rails/flight-booker/app/controllers/flights_controller.rb
+++ rails/flight-booker/app/controllers/flights_controller.rb
@@ -5,26 +5,22 @@ class FlightsController < ApplicationController
       @departure_options = Airport.all.map { |airport| [airport.code, airport.id] }
       @arrival_options = @departure_options
 
-      @start_datetimes = Flight.all.map { |flight| Date.parse(flight.start_datetime).to_fs(:long)  }
+      @start_datetimes = Flight.all.map { |flight| [Date.parse(flight.start_datetime).to_fs(:long), flight.start_datetime]  }
+
+      @flights = params[:flight] ? Flight.where(flight_search_params) : []
+
+      p @flight
+
+      if @flights
+        flash[:success] = "Results found"
+      else
+        flash[:notice] = "No rasults found"
+      end
     end
 
-    def show
-      @flight = flight.find(params[:id])
-    end
+    private
 
-    def create
-      @flight = flight.new(params[:flight])
-    end
-
-    def update
-      @flight = flight.find(params[:id])
-    end
-
-    def edit
-      @flight = flight.find(params[:id])
-    end
-
-    def destroy
-      flight = flight.find(params[:id])
+    def flight_search_params
+      params.require(:flight).permit(:departure_airport, :arrival_airport, :start_datetime)
     end
 end
