7819b39c01e19829333f7c22db00705e772eeda9
yaugesha <eugene.korchemenko@gmail.com>
2024-02-26 19:50:32 +0300

    feat: add seed data and render_to json method in index and show methods

--- rails/kittens/app/controllers/kittens_controller.rb
+++ rails/kittens/app/controllers/kittens_controller.rb
@@ -1,60 +1,16 @@
 class KittensController < ApplicationController
-  class UsersController < ApplicationController
-    before_action :find_user, except: [:index, :new, :create]
 
-    def index
-      @kittens = Kitten.all
+  def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.json  { render json: @kittens }
     end
-
-    def show
-      @kitten = Kitten.find(params[:id])
-    end
-
-    def new
-      @kitten = Kitten.new
-    end
-
-    def edit
-      @kitten = Kitten.new
-    end
-
-    def create
-      @kitten = Kitten.new(params[:user])
-      if @kitten.save
-        flash[:success] = "Kitten successfully created"
-        redirect_to @kitten
-      else
-        flash[:error] = "Something went wrong"
-        render 'new'
-      end
-    end
-
-    def update
-      if @kitten.update_attributes(params[:user])
-        flash[:success] = "Kitten was successfully updated"
-        redirect_to @kitten
-      else
-        flash[:error] = "Something went wrong"
-        render 'edit'
-      end
-    end
-
-    def destroy
-      if @kitten.destroy
-        flash[:success] = "Kitten was successfully deleted"
-        redirect_to @kittens_path
-      else
-        flash[:error] = "Something went wrong"
-        redirect_to @kittens_path
-      end
-    end
-
-    private
-
-      def find_kitten
-        @kitten = Kitten.find(params[:id])
-      end
-
   end
 
+  def show
+    @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      format.json  { render json: @kitten }
+    end
+  end
 end
--- rails/kittens/config/routes.rb
+++ rails/kittens/config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "kittens#index"
+  #root "kittens#index"
 
-  resources :kittens
+  resources :kittens, only: [:index, :show]
 
 end
